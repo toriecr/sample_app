791cdaac6b042581c6379d39ce60d83aa2b9782d
chinenra <chinenra@usc.edu>
2020-08-11 01:54:41 +0000

    Adding account activation 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_method = :smtp
   host = 'warm-bayou-61890.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
+    ​ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'heroku.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
