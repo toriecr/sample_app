e321551656fbe64520fea654987037076d251436
chinenra <chinenra@usc.edu>
2020-08-12 17:54:22 +0000

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
-  
-    
   include SessionsHelper
+  
+  private
+
+    # Confirms a logged-in user.
+    # Moved from UsersController to here since both Users and Microposts controller needs this method
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
   
 end
